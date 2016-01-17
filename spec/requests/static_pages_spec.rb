711c2a519e34cd8c8d063d04aa79cebe7a8b7dbd
Peter D. Duncan <pdduncan3@gmail.com>
2016-01-17 22:27:57 +0000

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,41 @@ require 'spec_helper'
 describe "Static pages, man," do
 
   describe "Home page" do
-    it "really ought to have the content 'Sample App'" do
+    it "really ought to have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "really ought to have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "yeah the Help page, man," do
-    it "really ought to contain 'help'" do
+    it "really ought to have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "you know, it should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-    it "really should have the content 'About Us'" do
+    it "really should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "really should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
